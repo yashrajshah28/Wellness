dce5f5fbb26b215db2b7782a131361b94cc1c71a
Freak <yashrajshah28@gmail.com>
2024-10-01 00:00:20 +0530

    sms update

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -3,29 +3,29 @@ import Image from "next/image";
 import Link from "next/link";
 import RegisterForm from "@/components/forms/RegisterForm";
 import { getUser } from "@/lib/actions/patient.actions";
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const Register = async ({ params: { userId } }: SearchParamProps) => {
   const user = await getUser(userId);
   Sentry.metrics.set("user_view_register", user.name);
-  
+
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container">
         <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
-          <Image
-            src="/assets/icons/logo-full.svg"
-            height={1000}
-            width={1000}
-            alt="patient"
-            className="mb-12 h-10 w-fit"
-          />
+          <Link href="/">
+            <Image
+              src="/assets/icons/logo-full.svg"
+              height={1000}
+              width={1000}
+              alt="patient"
+              className="mb-12 h-10 w-fit"
+            />
+          </Link>
 
           <RegisterForm user={user} />
 
-          <p className="copyright py-12">
-            © 2024 Wellness Wonders
-          </p>
+          <p className="copyright py-12">© 2024 Wellness Wonders</p>
         </div>
       </section>
       <Image
